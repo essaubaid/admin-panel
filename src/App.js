a13813ed03da18a07bb94c2a8172a74d59649383
vishalramiba <95579831+vishalramiba@users.noreply.github.com>
2022-05-31 18:03:07 +0500

    Order list done Vishal

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Login from "./pages/Login";
 function App() {
 
   const admin = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.role === "ADMIN"
+ //const admin =false
   return (
     <Router>
       <Switch>
@@ -49,6 +50,7 @@ function App() {
               <Route path="/orders">
                 <Orders />
               </Route>
+              
             </div>
           </>
         }
--- src/pages/orders/orders.jsx
+++ src/pages/orders/orders.jsx
@@ -1,11 +1,73 @@
 import "./orders.css";
-import WidgetLg from "../../components/widgetLg/WidgetLg";
+import { DataGrid } from "@material-ui/data-grid";
+import { DeleteOutline } from "@material-ui/icons";
+import { productRows } from "../../dummyData";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Orders() {
-    return (
-        <div className="orders">
-            <WidgetLg/>
-          </div>
-      );
-}
+  const [data, setData] = useState(productRows);
+  console.log(productRows)
 
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+        field: "customer",
+        headerName: "Customer",
+        width: 200,
+        renderCell: (params) => {
+          return (
+            <div className="orderListItem">
+              <img className="orderListImg" src={params.row.img} alt="" />
+              {params.row.name}
+            </div>
+          );
+        },
+      },
+      {
+        field: "date",
+        headerName: "Date",
+        width: 160,
+      },
+  
+    {
+        field: "amount",
+        headerName: "Amount",
+        width: 160,
+      },
+      { field: "transactionID", headerName: "Transaction ID", width: 200 },
+
+   
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={"/orders/" + params.row.id}>
+              <button className="orderListDetails">Details</button>
+            </Link>
+           
+          </>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div className="orderList">
+      <DataGrid
+        rows={data}
+        disableSelectionOnClick
+        columns={columns}
+        pageSize={8}
+        checkboxSelection
+      />
+    </div>
+  );
+}
