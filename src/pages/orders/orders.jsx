763db31f6d587ee02608054914bbe22a5f259ebd
essaubaid <essaubaid0@gmail.com>
2022-05-31 13:42:42 +0500

    first commit

---
+++ src/pages/orders/orders.jsx
@@ -0,0 +1,11 @@
+import "./orders.css";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
+
+export default function Orders() {
+    return (
+        <div className="orders">
+            <WidgetLg/>
+          </div>
+      );
+}
+
