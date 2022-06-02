4a1efaf216ccc7efee84e1042a198869cfb4ffdc
vishalramiba <95579831+vishalramiba@users.noreply.github.com>
2022-06-02 15:44:03 +0500

    topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,28 +1,26 @@
 import React from "react";
 import "./topbar.css";
-import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+import { Link } from 'react-router-dom';
+
 
 export default function Topbar() {
   return (
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
-          <span className="logo">lamaadmin</span>
+
+          <span className="logo">Admin Panel</span>
         </div>
-        <div className="topRight">
-          <div className="topbarIconContainer">
-            <NotificationsNone />
-            <span className="topIconBadge">2</span>
-          </div>
-          <div className="topbarIconContainer">
-            <Language />
-            <span className="topIconBadge">2</span>
-          </div>
-          <div className="topbarIconContainer">
-            <Settings />
-          </div>
-          <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
+        <div className="center">
+        <span className="logos">THRIFTY.</span>
         </div>
+        <Link to ="/login">
+        <button className="logoutButton">Logout</button>
+        </Link> 
+
+
+          
+        
       </div>
     </div>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { React, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { login } from '../redux/apiCalls'
+import { Link } from 'react-router-dom';
+
 
 const Login = () => {
     const [username, setUsername] = useState("")
@@ -17,9 +19,11 @@ const Login = () => {
         <div style={{ height: "100vh", display: "flex", alignItems: "center", flexDirection: "column", justifyContent: "center" }}>
             <input style={{ padding: 10, marginBottom: 20 }} type="text" placeholder='username' onChange={e => setUsername(e.target.value)}></input>
             <input style={{ padding: 10, marginBottom: 20 }} type="text" placeholder='password' onChange={e => setPassword(e.target.value)}></input>
+            <Link to="/home/">
             <button onClick={handleClick} style={{ padding: 10, width: 100 }}>
                 Login
             </button>
+            </Link>
         </div>
     )
 }
